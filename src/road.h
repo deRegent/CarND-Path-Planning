52d7fb0f40ec114fbb9e73ff09a084e287deb03c
Dmitry Zaganich <dmitryzaganich@gmail.com>
2018-03-15 18:44:41 +0200

    Git add road

---
+++ src/road.h
@@ -0,0 +1,34 @@
+//
+// Created by Дима on 15.03.2018.
+//
+
+#ifndef CARND_PATH_PLANNING_ROAD_H
+#define CARND_PATH_PLANNING_ROAD_H
+
+#include <math.h>
+#include "json.hpp"
+#include <map>
+
+#include "vehicle.h"
+
+namespace car_nd_path_planning {
+
+    using namespace std;
+
+    class Road {
+
+    public:
+        Road();
+
+        void update(json sensor_fusion);
+
+        Vehicle* get_closest_vehicle_ahead_of(Vehicle* vehicle);
+
+    private:
+        map<int, Vehicle*> vehicles;
+
+        double sensor_range = 400.0;
+    };
+}
+
+#endif //CARND_PATH_PLANNING_ROAD_H
