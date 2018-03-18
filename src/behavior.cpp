e1edb93b33b5a798d352d24484df877e7fc9a2a3
Dmitry Zaganich <dmitryzaganich@gmail.com>
2018-03-18 15:20:29 +0200

    fix speed limit

--- src/behavior.cpp
+++ src/behavior.cpp
@@ -234,18 +234,18 @@ namespace car_nd_path_planning {
 
     double Behavior::evaluate_lane_speed(int lane) {
         vector<double> speeds = road->get_speed_of_closest_vehicles_for(this->cur_vehicle);
+        for (int i=0; i < speeds.size(); i++){
+            if (speeds[i] > this->speed_limit) {
+                speeds[i] = this->speed_limit;
+            }
+        }
+
         double max = maxAt(speeds);
 
         printf("|Speed %d => %f|", 1, speeds[1]);
         printf("|Speed %d => %f|", 2, speeds[2]);
         printf("|Speed %d => %f|", 3, speeds[3]);
         printf("|Max => %f|", max);
-
-        if (max == road->empty_lane_speed) {
-            max = this->speed_limit;
-        } else if (max <= 0){
-            max = this->speed_limit;
-        }
 
         return 1 - (speeds[lane] / max);
     }
