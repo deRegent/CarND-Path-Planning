6bae8b3d0d4ba0ac80da05e895cea5804f03be0d
Dmitry Zaganich <dmitryzaganich@gmail.com>
2018-03-18 12:07:11 +0200

    debug

--- src/behavior.cpp
+++ src/behavior.cpp
@@ -224,11 +224,11 @@ namespace car_nd_path_planning {
                 return;
             }
 
-//            if ((best_lane - cur_lane) > 0) {
-//                this->state = State::PrepareLaneChangeRight;
-//            } else if ((best_lane - cur_lane) < 0) {
-//                this->state = State::PrepareLaneChangeLeft;
-//            }
+            if ((best_lane - cur_lane) > 0) {
+                this->state = State::PrepareLaneChangeRight;
+            } else if ((best_lane - cur_lane) < 0) {
+                this->state = State::PrepareLaneChangeLeft;
+            }
         }
     }
 
